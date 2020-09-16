5959c1441b5447cc74edc9e19b9e5769121eed03
Alex <danilaodessa@gmail.com>
2020-09-16 09:40:32 +0000

    some changes

--- project/core/base/controller/BaseController.php
+++ project/core/base/controller/BaseController.php
@@ -7,61 +7,66 @@ use Exception;
 
 abstract class BaseController
 {
-protected $page;
-protected $errors;
+    protected $page;
+    protected $errors;
 
     protected $controller;
     protected $inputMethod;
     protected $outputMethod;
     protected $parameters;
 
-public function route(){
+    public function route()
+    {
 
-    $controller = str_replace('/', '\\', $this->controller);
+        $controller = str_replace('/', '\\', $this->controller);
 
-    try{
-        $object = new \ReflectionMethod($controller,'request');
+        try {
+            $object = new \ReflectionMethod($controller, 'request');
 
-        $args = [
-            'parameters' => $this->parameters,
-            'outputMethod' =>$this->outputMethod,
-            'inputMethod' =>$this->inputMethod
-        ];
-        
-        $object ->invoke(new $controller, $args);
+            $args = [
+                'parameters' => $this->parameters,
+                'outputMethod' => $this->outputMethod,
+                'inputMethod' => $this->inputMethod
+            ];
 
-    }catch(\ReflectionException $e){
+            $object->invoke(new $controller, $args);
 
-        throw new  RouteException($e->getMessage());
+        } catch (\ReflectionException $e) {
 
+            throw new  RouteException($e->getMessage());
+
+        }
     }
-}
 
-    public function request($args){
+    public function request($args)
+    {
 
-        $this->parameters =$args['parameters'];
-        $outputData =$args['outputMethod'];
-        $inputData =$args['inputMethod'];
+        $this->parameters = $args['parameters'];
+        $outputData = $args['outputMethod'];
+        $inputData = $args['inputMethod'];
         $this->$inputData();
-       
+
         $this->page = $this->$outputData();
 
-        if($this->errors){
+        if ($this->errors) {
             $this->writeLog();
         }
 
         $this->getpage();
-}
-protected function render($path = '', $parameters = []){
-extract($parameters);
-if(!$path){
-    $path = TEMPLATE . explode('controller', strtolower((new \ReflectionClass($this))->getShortName() ))[0];
-dd($path);
-}
-}
+    }
 
-protected function getPage(){
-    exit($this->page);
-}
-    
+    protected function render($path = '', $parameters = [])
+    {
+        extract($parameters);
+        if (!$path) {
+            $path = TEMPLATE . explode('controller', strtolower((new \ReflectionClass($this))->getShortName()))[0];
+            dd($path);
+        }
+    }
+
+    protected function getPage()
+    {
+        exit($this->page);
+    }
+
 }
--- project/core/base/controller/RouteController.php
+++ project/core/base/controller/RouteController.php
@@ -4,122 +4,96 @@ namespace base\controller;
 
 use base\exceptions\RouteException;
 use base\settings\Settings;
-use base\settings\ShopSettings;
 
 class RouteController extends BaseController
 {
     static private $_instance;
     protected $routes;
 
-   
-
 
     private function __construct()
     {
 
-      $adress_str= $_SERVER['REQUEST_URI'];
+        $address_str = $_SERVER['REQUEST_URI'];
 
-      if (strrpos($adress_str, '/') === strlen($adress_str)-1 && strrpos($adress_str, '/') !==0) {
-       $this->redirect(rtrim($adress_str, '/'), 301);
-      }
+        if (strrpos($address_str, '/') === strlen($address_str) - 1 && strrpos($address_str, '/') !== 0) {
+            $this->redirect(rtrim($address_str, '/'), 301);
+        }
 
-      $path =substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php'));
+        $path = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php'));
 
-      if($path === PATH){
-      
-          $this->routes = Settings::get('routes');
-        
-          if(!$this->routes) throw new RouteException('route exeption');
+        if ($path === PATH) {
 
-          $url =explode('/',substr($adress_str, strlen(PATH)));
-          
-          if($url[0] && $url[0] === $this->routes['admin']['alias']){
-          
-            $plugin = array_shift($url);
+            $this->routes = Settings::get('routes');
 
-            if($url[0] && is_dir($_SERVER['DOCUMENT_ROOT'].PATH . $this->routes['plugins']['path'] . $url[0])){
-               
-                $plugin = array_shift($url);
-               
-                $pluginSettings = $this->routes['settings']['path'] . ucfirst($plugin).'Settings';
-                
-                if(file_exists($_SERVER['DOCUMENT_ROOT'] . PATH . $pluginSettings . '.php')){
-                   
-                    $pluginSettings = str_replace('/' , '\\', $pluginSettings);
-                    
-                    $this->routes = $pluginSettings::get('routes');
-                 }
-                 
-                $dir = $this->routes['plugins']['dir'] ? '/' . $this->routes['plugins']['dir'] . '/' :'/';
-                
-                $dir = str_replace('//', '/', $dir);
-
-                $this->controller =$this->routes['plugins']['path'] . $plugin . $dir;
-               
-
-                $hrUrl =$this->routes['plagin']['hrUrl'];
-
-                $route ='plugins';
-
-            }else{
-
-                 $this->controller = $this->routes['admin']['path'];
-
-                 $hrUrl =$this->routes['admin']['hrUrl'];
-
-                 $route ='admin';
-
+            if (!$this->routes) {
+                throw new RouteException('route exception');
             }
 
-          }else{
+            $url = explode('/', substr($address_str, strlen(PATH)));
 
-             
-              
-              $hrUrl=$this->routes['user']['hrUrl'];
+            if ($url[0] && $url[0] === $this->routes['admin']['alias']) {
 
-              $this->controller =$this->routes['user']['path'];
 
-              $route ='user';
-          }
-        
-      
-          $this->createRout($route,  $url);
-          if($url[1]){
-              $count = count($url);
-              $key ='';
-              if(!$hrUrl){
-                  $i=1;
-              }else{
-                  $this->parameters['alias'] = $url [1];
-                  $i=2;
-              }
+                if ($url[0] && is_dir($_SERVER['DOCUMENT_ROOT'] . PATH . $this->routes['plugins']['path'] . $url[0])) {
+                    $plugin = array_shift($url);
+                    $pluginSettings = $this->routes['settings']['path'] . ucfirst($plugin) . 'Settings';
 
-              for( ; $i < $count; $i++){
-                
-                if(!$key){
-                   
-                    $key = $url[$i];
-                    
-                    $this->parameters[$key] = '';
-                }else{
-                    
-                   
-                    $this->parameters[$key] = $url[$i];
-                    
-                    $key = '';
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . PATH . $pluginSettings . '.php')) {
+                        $pluginSettings = str_replace('/', '\\', $pluginSettings);
+                        $this->routes = $pluginSettings::get('routes');
+                    }
+
+                    $dir = $this->routes['plugins']['dir'] ? '/' . $this->routes['plugins']['dir'] . '/' : '/';
+                    $dir = str_replace('//', '/', $dir);
+                    $this->controller = $this->routes['plugins']['path'] . $plugin . $dir;
+                    $hrUrl = $this->routes['plugins']['hrUrl'];
+                    $route = 'plugins';
+
+                } else {
+                    $this->controller = $this->routes['admin']['path'];
+                    $hrUrl = $this->routes['admin']['hrUrl'];
+                    $route = 'admin';
+
                 }
 
-              }
+            } else {
+                $hrUrl = $this->routes['user']['hrUrl'];
+                $this->controller = $this->routes['user']['path'];
+                $route = 'user';
             }
-        
-      }else{
-          try{
-              throw new \Exception('bad site dir');
-          }
-          catch(\Exception $e){
-               exit($e->getMessage());
-          }
-      }
+
+
+            $this->createRout($route, $url);
+            if ($url[1]) {
+                $count = count($url);
+                $key = '';
+                if (!$hrUrl) {
+                    $i = 1;
+                } else {
+                    $this->parameters['alias'] = $url [1];
+                    $i = 2;
+                }
+
+                for (; $i < $count; $i++) {
+
+                    if (!$key) {
+                        $key = $url[$i];
+                        $this->parameters[$key] = '';
+                    } else {
+                        $this->parameters[$key] = $url[$i];
+                        $key = '';
+                    }
+                }
+            }
+
+        } else {
+            try {
+                throw new \Exception('bad site dir');
+            } catch (\Exception $e) {
+                exit($e->getMessage());
+            }
+        }
     }
 
     private function __clone()
@@ -134,28 +108,25 @@ class RouteController extends BaseController
         return self::$_instance = new self();
     }
 
-    private function createRout($var, $arr){
-        $route=[];
-       
-        if(!empty($arr[0])){
-            
-           
-           if($this->routes[$var]['routes'][$arr[0]]){
-           
-               $route = explode('/', $this->routes[$var]['routes'][$arr[0]]);
-               $this->controller .= ucfirst($route[0].'Controller');
-           }else{
-               
-               $this->controller .= ucfirst($arr[0].'Controller');
-               
-           }
-        }else{
-            
+    private function createRout($var, $arr)
+    {
+        $route = [];
+
+        if (!empty($arr[0])) {
+            if ($this->routes[$var]['routes'][$arr[0]]) {
+                $route = explode('/', $this->routes[$var]['routes'][$arr[0]]);
+                $this->controller .= ucfirst($route[0] . 'Controller');
+            } else {
+
+                $this->controller .= ucfirst($arr[0] . 'Controller');
+
+            }
+        } else {
             $this->controller .= $this->routes['default']['controller'];
         }
-      
-        $this->inputMethod =$route[1] ? $route[1] : $this->routes['default']['inputMethod'];
-        $this->outputMethod =$route[2] ? $route[2] : $this->routes['default']['outputMethod'];
+
+        $this->inputMethod = $route[1] ? $route[1] : $this->routes['default']['inputMethod'];
+        $this->outputMethod = $route[2] ? $route[2] : $this->routes['default']['outputMethod'];
         return;
 
     }
