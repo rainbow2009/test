1e9e2b51263fd4721a228cbb6b72f16a2ac079fb
Alex <danilaodessa@gmail.com>
2020-10-06 15:57:54 +0300

    1.5

--- project/core/admin/controller/IndexController.php
+++ project/core/admin/controller/IndexController.php
@@ -14,80 +14,6 @@ class IndexController extends BaseController
     protected function inputData()
     {
 
-<<<<<<< HEAD
-
-        $db = Model::instance();
-
-
-        $table = 'teacher';
-
-
-        // for ($i = 0; $i < 8; $i++) {
-        //     $s_id = $db->add('students', [
-        //         'fields' => ['name' => 'student - ' . $i, 'content' => 'content - ' . $i],
-        //         'return_id' => true
-        //     ]);
-        //     $db->add('teacher', [
-        //         'fields' => ['name' => 'teacher - ' . $i, 'student_id' => $s_id]
-        //     ]);
-        // }
-$res = $db->delete($table, [
-              'where' => [ 'id' => '16'  ],
-              'join' =>[
-                  [ 'table' => 'students',
-                      'on' =>['student_id','id']
-                  ]
-              ],
-]);
-
-//        $res = $db->get($table,
-//            [
-//                'fields' => ['id','name'],
-//                'where' => [ 'name' => 'masha'  ],
-//                 'operand' =>['IN', '<>'],
-//                 'condition' => ["OR",'AND'],
-//                'order' => [ 'name'],
-//                'order_direction' => ['DESC'],
-//                'limit' => '1',
-        // 'join' =>[
-        //   [
-        //   'table' => 'join_table1',
-        //   'fields' => ['id as j_id','name as j_name'],
-        //   'type' =>'left',
-        //   'where' => [ 'name' => 'sasha'],
-        //   'operand' =>['='],
-        //   'condition' => ["OR"],
-        //   'on' => [
-        //    'table' => 'teachers',
-        //    'fields' => ['id', 'parent_id']
-        //   ]
-        // ],
-        // 'join_table2' =>[
-        //     'table' => 'join_table2',
-        //     'fields' => ['id as j2_id','name as j2_name'],
-        //     'type' =>'left',
-        //     'where' => [ 'name' => 'sasha'],
-        //     'operand' =>['<>'],
-        //    'condition' => ["AND"],
-        //     'on' =>['id', 'parent_id']
-
-        // ]
-//            ]
-//        ]
-//    );
-
-
-        dd(1);
-        $name = 'masha';
-        $surname = 'vasay';
-        $this->name = 'croco';
-
-        $content = $this->render(ADMIN_TEMPLATE . 'content', compact('name'));
-        $header = $this->render(ADMIN_TEMPLATE . 'header');
-        $footer = $this->render(ADMIN_TEMPLATE . 'footer');
-
-        return compact('header', 'content', 'footer');
-=======
 $redirect = PATH.Settings::get('routes')['admin']['alias'].'/show';
 $this->redirect($redirect);
 
@@ -155,7 +81,6 @@ $this->redirect($redirect);
 //        $footer = $this->render(ADMIN_TEMPLATE . 'footer');
 //
 //        return compact('header', 'content', 'footer');
->>>>>>> 06bc4f3f64d6641850b4fb7eb80fcd9642f47f39
     }
 
     protected function outputData()
