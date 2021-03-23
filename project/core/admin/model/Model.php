b65a13b8e9970a67e8c1a11005e79452abe59c70
Alex <danilaodessa@gmail.com>
2021-03-23 16:47:40 +0200

    updateMenuPos

--- project/core/admin/model/Model.php
+++ project/core/admin/model/Model.php
@@ -35,15 +35,65 @@ class Model extends BaseModel
     {
         if ($update_rows && isset($update_rows['where'])) {
 
-            $update_rows['operand'] = isset($update_rows['operand']) ? $update_rows['operand'] :['='];
+            $update_rows['operand'] = isset($update_rows['operand']) ? $update_rows['operand'] : ['='];
 
-            if($where){
+            if ($where) {
 
-                $old_data =$this->get($table, [
-                    'fields' => [$update_rows['where'],$row],
+                $old_data = $this->get($table, [
+                    'fields' => [$update_rows['where'], $row],
                     'where' => $where
                 ])[0];
+
+                $start_pos = $old_data[$row];
+                if ($old_data[$update_rows['where']] !== $_POST[$update_rows['where']]) {
+
+                    $pos = $this->get($table, [
+                        'fields' => ['COUNT(*) as count'],
+                        'where' => [$update_rows['where'] => $old_data[$update_rows['where']]],
+                        'no_concat' => true,
+                    ])[0]['count'];
+
+                    if ($start_pos != $pos) {
+                        $update_where = $this->createWhere([
+                            'where' => [$update_rows['where'] => $old_data[$update_rows['where']]],
+                            'operand' => $update_rows['operand']
+                        ]);
+
+                        $query = "UPDATE $table SET $row = $row - 1 $update_where AND $row <= $pos AND $row > $start_pos";
+                        $this->query($query, 'u');
+
+                    }
+
+                    $start_pos = $this->get($table, [
+                            'fields' => ['COUNT(*) as count'],
+                            'where' => [$update_rows['where'] => $_POST[$update_rows['where']]],
+                            'no_concat' => true,
+                        ])[0]['count'] + 1;
+
+                }
+
+            } else {
+
+                $start_pos = $this->get($table, [
+                        'fields' => ['COUNT(*) as count'],
+                        'where' => [$update_rows['where'] => $_POST[$update_rows['where']]],
+                        'no_concat' => true,
+                    ])[0]['count'] + 1;
             }
+
+            if (array_key_exists($update_rows['where'], $_POST)) {
+                $where_equal = $_POST[$update_rows['where']];
+            } elseif (isset($old_data[$update_rows['where']])) {
+                $where_equal = $old_data[$update_rows['where']];
+            } else {
+                $where_equal = NULL;
+            }
+
+            $db_where = $this->createWhere([
+                'where' => [$update_rows['where'] => $where_equal],
+                'operand' => $update_rows['operand']
+            ]);
+
 
         } else {
             if ($where) {
@@ -58,6 +108,17 @@ class Model extends BaseModel
                     ])[0]['count'] + 1;
             }
         }
+
+        $db_where = isset($db_where) ? $where_equal . ' AND' : 'WHERE';
+
+        if ($start_pos < $end_pos) {
+            $query = "UPDATE $table SET $row = $row -1 $db_where $row <= $end_pos  AND $row > $start_pos";
+        }elseif ($start_pos > $end_pos){
+            $query = "UPDATE $table SET $row = $row +1 $db_where $row >= $end_pos  AND $row < $start_pos";
+        }else return;
+
+        return $this->query($query,'u');
+
     }
 
 }
